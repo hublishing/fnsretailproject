ea760925d9b6a9332357baeb5084569b510a7985
han <jaehun_@kakao.com>
2024-09-10 09:54:10 +0900

    h

--- public/js/aws-exports.js
+++ public/js/aws-exports.js
@@ -1,9 +1,8 @@
-const awsmobile = {
+Amplify.configure({
     Auth: {
-        userPoolId: 'ap-northeast-2_4BBJiRrR9',
-        userPoolWebClientId: '122g2i26b7erslg90ut69s227l',
-        region: 'ap-northeast-2',
+      userPoolId: 'ap-northeast-2_4BBJiRrR9', // 사용자 풀 ID
+      userPoolWebClientId: '122g2i26b7erslg90ut69s227l', // 클라이언트 ID
+      region: 'ap-northeast-2', // 리전
     }
-};
-
-export default awsmobile;
+  });
+  

9ead8a5474b5606716cd6a652a54410a51b2f16e
han <jaehun_@kakao.com>
2024-09-10 12:14:17 +0900

    a

--- public/js/join.js
+++ public/js/join.js
@@ -1,5 +1,5 @@
 // AWS Amplify 설정
-Amplify.configure({
+Amplify.default.configure({
   Auth: {
     userPoolId: 'ap-northeast-2_4BBJiRrR9', // 사용자 풀 ID
     userPoolWebClientId: '122g2i26b7erslg90ut69s227l', // 클라이언트 ID

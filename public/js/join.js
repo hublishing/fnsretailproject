d363841bf7be2667760631878f7590a0adbb5f41
han <jaehun_@kakao.com>
2024-09-10 12:27:35 +0900

    a

--- public/js/join.js
+++ public/js/join.js
@@ -1,7 +1,5 @@
 // AWS Amplify 설정
 console.log("Amplify 설정을 시작합니다.");
-import Amplify from 'aws-amplify';
-import awsconfig from './aws-exports';  // AWS 설정을 포함한 파일
 
 Amplify.configure({
   Auth: {
